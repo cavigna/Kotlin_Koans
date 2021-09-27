67949617c45de4855250e366ff6e7323a5c540f7
cavigna <40524472+cavigna@users.noreply.github.com>
2021-09-27 16:22:00 -0300

    Initial commit

---
+++ Introduction/Named arguments/src/Task.kt
@@ -0,0 +1,8 @@
+fun joinOptions(options: Collection<String>
+) =
+        options.joinToString(prefix = "[", postfix = "]")
+
+/*
+fun joinOptions(options: Collection<String>) =
+        options.joinToString(TODO())
+ */
